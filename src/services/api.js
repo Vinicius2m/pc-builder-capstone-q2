f0b78763e41f6b91342506cea8cb2a04c58835d3
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2023-05-01 23:57:35 -0300

    feat: adding new api base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://pc-builder-json-api.herokuapp.com",
+  baseURL: "https://pc-builder-json-server-production.up.railway.app",
 });
 
 export default api;
