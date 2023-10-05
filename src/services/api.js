729c8f7f33fa2037abea025c97e22c97dbd8f528
Vinícius de Freitas <viniciusfreitas.1.4.4@gmail.com>
2023-10-05 13:12:05 -0300

    feat: adding api base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://pc-builder-json-server-production.up.railway.app",
+  baseURL: "https://pc-builder-json-server.onrender.com",
 });
 
 export default api;
